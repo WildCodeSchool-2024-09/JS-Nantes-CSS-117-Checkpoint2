5d46a4d7b31e93bf5fc00c92a2ce80d7980421b7
CyrusWeir <cyrusweir@gmail.com>
2024-12-02 12:46:21 +0100

    je me suis rendu à l'étape 5.

--- client/src/pages/CupcakeList.tsx
+++ client/src/pages/CupcakeList.tsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Cupcake from "../components/Cupcake";
 
 /* ************************************************************************* */
@@ -34,15 +34,47 @@ const sampleCupcakes = [
 
 type CupcakeArray = typeof sampleCupcakes;
 
+type Accessories = {
+  id: number;
+  name: string;
+  slug: string;
+}[];
+
 /* you can use sampleCupcakes if you're stucked on step 1 */
 /* if you're fine with step 1, just ignore this ;) */
 /* ************************************************************************* */
 
+interface CupcakeArrayPlus extends CupcakeArray {
+  cupcakeArray: CupcakeArray[];
+  setCupcakeArray: (cupcakeArray: CupcakeArray) => void;
+}
+
 function CupcakeList() {
+  const [accessories, setAccessories] = useState<Accessories>();
+  const [cupcakeArray, setCupcakeArray] = useState<CupcakeArrayPlus>();
   // Step 1: get all cupcakes
-  console.info(useLoaderData() as CupcakeArray);
+  useEffect(() => {
+    const url = "http://localhost:3310/api/cupcakes";
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCupcakeArray(data);
+      });
+  }, []);
+  console.info(cupcakeArray);
 
   // Step 3: get all accessories
+  useEffect(() => {
+    const url = "http://localhost:3310/api/accessories";
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setAccessories(data);
+      });
+  }, []);
+  console.info(accessories);
 
   // Step 5: create filter state
 
@@ -52,19 +84,28 @@ function CupcakeList() {
       <form className="center">
         <label htmlFor="cupcake-select">
           {/* Step 5: use a controlled component for select */}
-          Filter by{" "}
+          Filter by{"accessory"}
           <select id="cupcake-select">
-            <option value="">---</option>
+            {accessories?.map((el) => (
+              <option key={el.id} value={el.name}>
+                {el.slug}
+              </option>
+            ))}
             {/* Step 4: add an option for each accessory */}
           </select>
         </label>
       </form>
       <ul className="cupcake-list" id="cupcake-list">
-        {/* Step 2: repeat this block for each cupcake */}
+        {/* Step 2: repeat this block for each cupcake - NOTE: j'avais une erreur 'undefined' quand j'ai essayé d'utiliser le data que j'ai fetché - j'imagine que c'est a cause de la manque du loader ? */}
+        {cupcakeArray?.map((el) => (
+          <li key={el.id} className="cupcake-item">
+            <Cupcake data={el} />
+          </li>
+        ))}
         {/* Step 5: filter cupcakes before repeating */}
-        <li className="cupcake-item">
+        {/* {<li className="cupcake-item">
           <Cupcake data={sampleCupcakes[0]} />
-        </li>
+        </li> } */}
         {/* end of block */}
       </ul>
     </>
