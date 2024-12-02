8867fe6e0afcc81ff6d3491e6e2447920cdc4c9d
Clément Praud <clement.praud6@gmail.com>
2024-12-02 14:56:12 +0100

    finish algo 4 for the fun

--- algo/4/sumArrays.ts
+++ algo/4/sumArrays.ts
@@ -1,6 +1,7 @@
 /*
 Écris une fonction avec deux paramètres. Ces paramètres sont des tableaux contenant des nombres **stockés sous forme de chaînes de caractères**.
-Ta fonction doit renvoyer **un** tableau, où chaque élément est la somme des éléments des deux arguments correspondants (c'est-à-dire : le premier élément du tableau résultat est égal au premier élément du premier paramètre plus le premier élément du deuxième paramètre) .
+Ta fonction doit renvoyer **un** tableau, où chaque élément est la somme des éléments des deux arguments correspondants (c'est-à-dire : le premier élément 
+du tableau résultat est égal au premier élément du premier paramètre plus le premier élément du deuxième paramètre) .
 Remarque : Si un élément est vide, il doit compter pour 0.
 Ex: 
 sumArr( ["1", "2", "3"], ["2", "4", "1"] ) should return ["3", "6", "4"]
@@ -10,8 +11,46 @@ sumArr( ["2", "5", "3"], ["2", "4", "9", "5", "5"] ) should return ["4", "9", "1
 */
 
 function sumArr(arrayA: string[], arrayB: string[]): string[] {
-  // Your code here !
-  return [];
+  const result = [];
+  let lengthA = 0;
+  let lengthB = 0;
+  const tableA = [];
+  const tableB = [];
+  let sumArray = 0;
+
+  for (const string in arrayA) {
+    const splittedArrayA = arrayA[string].split("");
+    tableA.push(splittedArrayA);
+  }
+
+  lengthA = tableA.length;
+
+  for (const string in arrayB) {
+    const splittedArrayB = arrayB[string].split("");
+    tableB.push(splittedArrayB);
+  }
+  lengthB = tableB.length;
+
+  if (lengthA >= lengthB) {
+    for (let i = 0; i < lengthA; i++) {
+      if (tableB[i]) {
+        sumArray = Number(tableA[i]) + Number(tableB[i]);
+        result.push(sumArray.toString());
+      } else {
+        result.push(Number(tableA[i]).toString());
+      }
+    }
+  } else {
+    for (let i = 0; i < lengthB; i++) {
+      if (tableA[i]) {
+        sumArray = Number(tableA[i]) + Number(tableB[i]);
+        result.push(sumArray.toString());
+      } else {
+        result.push(Number(tableB[i]).toString());
+      }
+    }
+  }
+  return result;
 }
 
 export default sumArr;
